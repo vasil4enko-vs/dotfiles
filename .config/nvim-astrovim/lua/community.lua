0aa6214bec4dea4d47583ec3a3fed60b299c360f
vasil4enko-vs <vvs170@gmail.com>
2025-03-30 17:50:54 +0300

    nvim-astrovim.OLD переименовал в nvim-astrovim-4

--- .config/nvim-astrovim/lua/community.lua
+++ .config/nvim-astrovim/lua/community.lua
@@ -8,7 +8,30 @@
 return {
   "AstroNvim/astrocommunity",
   -- import/override with your plugins folder
-  -- Language Pack
+
+  -- Bars & Lines
+  -- { import = "astrocommunity.bars-and-lines.bufferline-nvim" }, -- A snazzy bufferline for Neovim (Note: This also disables the Heirline based tabline)
+  { import = "astrocommunity.bars-and-lines.dropbar-nvim" }, -- IDE-like breadcrumbs, out of the box (Note: This also disables the Heirline based winbar)
+
+  -- Code Ranners
+  { import = "astrocommunity.code-runner.compiler-nvim" }, -- Compiler for building and running code
+
+  -- Colorscheme/Theme
+  { import = "astrocommunity.recipes.cache-colorscheme" }, -- Cache Colorscheme
+
+  -- Diagnostics
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+
+  -- Editing support
+  { import = "astrocommunity.editing-support.neogen" }, -- Annotation Toolkit
+  { import = "astrocommunity.editing-support.multiple-cursors-nvim" }, -- Multi-cursor plugin
+  { import = "astrocommunity.editing-support.nvim-treesitter-context" }, -- Show code context
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" }, -- Rainbow delimiters for Neovim with Tree-sitter
+
+  -- Media
+  { import = "astrocommunity.media.vim-wakatime" },
+
+  -- Pack Language
   { import = "astrocommunity.pack.lua" }, -- Lua
   { import = "astrocommunity.pack.bash" }, -- Bash
   { import = "astrocommunity.pack.python-ruff" }, -- Python
@@ -19,26 +42,7 @@ return {
   { import = "astrocommunity.markdown-and-latex.peek-nvim" }, -- Markdown preview plugin for Neovim
   { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" }, -- Plugin to improve viewing Markdown files in Neovim
   { import = "astrocommunity.editing-support.telescope-undo-nvim" }, -- View and search your undo tree
-  -- Editing support
-  { import = "astrocommunity.editing-support.neogen" }, -- Annotation Toolkit
-  { import = "astrocommunity.editing-support.multiple-cursors-nvim" }, -- Multi-cursor plugin
-  { import = "astrocommunity.editing-support.nvim-treesitter-context" }, -- Show code context
-  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" }, -- Rainbow delimiters for Neovim with Tree-sitter
 
-  -- Diagnostics
-  { import = "astrocommunity.diagnostics.trouble-nvim" },
-
-  -- Code Ranners
-  { import = "astrocommunity.code-runner.compiler-nvim" }, -- Compiler for building and running code
-
-  -- Media
-  { import = "astrocommunity.media.vim-wakatime" },
-
-  ---- UI
-  -- Bars & Lines
-  -- { import = "astrocommunity.bars-and-lines.bufferline-nvim" }, -- A snazzy bufferline for Neovim (Note: This also disables the Heirline based tabline)
-  { import = "astrocommunity.bars-and-lines.dropbar-nvim" }, -- IDE-like breadcrumbs, out of the box (Note: This also disables the Heirline based winbar)
-  -- Colorscheme/Theme
-  { import = "astrocommunity.recipes.cache-colorscheme" }, -- Cache Colorscheme
+  --Recipe
   { import = "astrocommunity.colorscheme.catppuccin" }, -- Catppuccin
 }
