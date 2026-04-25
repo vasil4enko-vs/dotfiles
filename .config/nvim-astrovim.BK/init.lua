f981885d355a09538d0e2291cac929bd7971e27f
vasil4enko-vs <vvs170@gmail.com>
2026-04-25 15:15:53 +0300

    Изменение расположения

--- .config/nvim-astrovim.BK/init.lua
+++ .config/nvim-astrovim.BK/init.lua
@@ -1,18 +1,10 @@
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
 if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   -- stylua: ignore
-  local result = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
-  if vim.v.shell_error ~= 0 then
-    -- stylua: ignore
-    vim.api.nvim_echo({ { ("Error cloning lazy.nvim:\n%s\n"):format(result), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
-    vim.fn.getchar()
-    vim.cmd.quit()
-  end
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-
 vim.opt.rtp:prepend(lazypath)
 
 -- validate that lazy is available
@@ -25,3 +17,17 @@ end
 
 require "lazy_setup"
 require "polish"
+
+-- Inclusion of the display of invisible symbols
+vim.opt.list = true -- Enable showing invisible characters
+vim.opt.listchars:append {
+  eol = "↲", -- Symbol for end of line
+  -- Other symbols (optional):
+  tab = "▸ ", -- Tab character
+  trail = "•", -- Trailing spaces
+  nbsp = "␣", -- Non-breaking spaces
+  -- space = "•", -- Spaces
+  extends = "<", -- The text continues behind the right border of the window
+  precedes = ">", -- The text begins to the left of the visible area
+  -- conceal = "┊", -- Symbol for hidden text (requires 'Conceallevel')
+}
